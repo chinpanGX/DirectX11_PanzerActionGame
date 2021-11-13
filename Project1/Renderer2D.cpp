c5687c3e3dfcee12071c013702b58baa7d5b4bf0
ChinpanGX <xchuhe@gameil.com>
2021-11-13 16:41:16 +0900

    no message

--- Project1/Renderer2D.cpp
+++ Project1/Renderer2D.cpp
@@ -14,25 +14,25 @@ Renderer2D::Renderer2D(Graphics & graphics, Resource& resource, const std::strin
 	: m_Graphics(graphics), m_Resource(resource), m_Texture(Tag)
 {
 	Vertex3D vertex[4];
-	vertex[0].Position = DirectX::XMFLOAT3(-1.0f, 1.0f, 0.0f);
-	vertex[0].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[0].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[0].TexCoord = DirectX::XMFLOAT2(0.0f, 0.0f);
+	vertex[0].Position = D3DXVECTOR3(-1.0f, 1.0f, 0.0f);
+	vertex[0].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[0].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[0].TexCoord = D3DXVECTOR2(0.0f, 0.0f);
 
-	vertex[1].Position = DirectX::XMFLOAT3(1.0f, 1.0f, 0.0f);
-	vertex[1].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[1].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[1].TexCoord = DirectX::XMFLOAT2(1.0f, 0.0f);
+	vertex[1].Position = D3DXVECTOR3(1.0f, 1.0f, 0.0f);
+	vertex[1].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[1].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[1].TexCoord = D3DXVECTOR2(1.0f, 0.0f);
 
-	vertex[2].Position = DirectX::XMFLOAT3(-1.0f, -1.0f, 0.0f);
-	vertex[2].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[2].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[2].TexCoord = DirectX::XMFLOAT2(0.0f, 1.0f);
+	vertex[2].Position = D3DXVECTOR3(-1.0f, -1.0f, 0.0f);
+	vertex[2].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[2].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[2].TexCoord = D3DXVECTOR2(0.0f, 1.0f);
 
-	vertex[3].Position = DirectX::XMFLOAT3(1.0f, -1.0f, 0.0f);
-	vertex[3].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[3].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[3].TexCoord = DirectX::XMFLOAT2(1.0f, 1.0f);
+	vertex[3].Position = D3DXVECTOR3(1.0f, -1.0f, 0.0f);
+	vertex[3].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[3].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[3].TexCoord = D3DXVECTOR2(1.0f, 1.0f);
 
 
 	//頂点バッファ生成
@@ -51,29 +51,29 @@ Renderer2D::Renderer2D(Graphics & graphics, Resource& resource, const std::strin
 	ThrowIfFailed(hr, "m_VertexBuffer is Failed");
 }
 
-Renderer2D::Renderer2D(Graphics & graphics, Resource & resource, const std::string & Tag, const Math::Vector2 & pos, const Math::Vector2 & size, const Math::Vector2 & ul, const Math::Vector2 & lr)
+Renderer2D::Renderer2D(Graphics & graphics, Resource & resource, const std::string & Tag, const D3DXVECTOR2 & pos, const D3DXVECTOR2 & size, const D3DXVECTOR2 & ul, const D3DXVECTOR2 & lr)
 	: m_Graphics(graphics), m_Resource(resource), m_Texture(Tag)
 {
 	Vertex3D vertex[4];
-	vertex[0].Position = DirectX::XMFLOAT3(pos.x - size.x * 0.5f, pos.y - size.y * 0.5f, 0.0f);
-	vertex[0].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[0].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[0].TexCoord = DirectX::XMFLOAT2(ul.x, ul.y);
+	vertex[0].Position = D3DXVECTOR3(pos.x - size.x * 0.5f, pos.y - size.y * 0.5f, 0.0f);
+	vertex[0].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[0].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[0].TexCoord = D3DXVECTOR2(ul.x, ul.y);
 
-	vertex[1].Position = DirectX::XMFLOAT3(pos.x + size.x * 0.5f, pos.y - size.y * 0.5f, 0.0f);
-	vertex[1].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[1].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[1].TexCoord = DirectX::XMFLOAT2(lr.x, ul.y);
+	vertex[1].Position = D3DXVECTOR3(pos.x + size.x * 0.5f, pos.y - size.y * 0.5f, 0.0f);
+	vertex[1].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[1].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[1].TexCoord = D3DXVECTOR2(lr.x, ul.y);
 
-	vertex[2].Position = DirectX::XMFLOAT3(pos.x - size.x * 0.5f, pos.y + size.y * 0.5f, 0.0f);
-	vertex[2].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[2].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[2].TexCoord = DirectX::XMFLOAT2(ul.x, lr.y);
+	vertex[2].Position = D3DXVECTOR3(pos.x - size.x * 0.5f, pos.y + size.y * 0.5f, 0.0f);
+	vertex[2].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[2].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[2].TexCoord = D3DXVECTOR2(ul.x, lr.y);
 
-	vertex[3].Position = DirectX::XMFLOAT3(pos.x + size.x * 0.5f, pos.y + size.y * 0.5f, 0.0f);
-	vertex[3].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[3].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[3].TexCoord = DirectX::XMFLOAT2(lr.x, lr.y);
+	vertex[3].Position = D3DXVECTOR3(pos.x + size.x * 0.5f, pos.y + size.y * 0.5f, 0.0f);
+	vertex[3].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[3].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[3].TexCoord = D3DXVECTOR2(lr.x, lr.y);
 
 	//頂点バッファ生成
 	D3D11_BUFFER_DESC bd;
@@ -96,28 +96,28 @@ Renderer2D::~Renderer2D()
 
 }
 
-void Renderer2D::SetVertex(const Math::Vector2 & pos, const Math::Vector2 & size, const Math::Vector2 & ul, const Math::Vector2 & lr)
+void Renderer2D::SetVertex(const D3DXVECTOR2 & pos, const D3DXVECTOR2 & size, const D3DXVECTOR2 & ul, const D3DXVECTOR2 & lr)
 {
 	Vertex3D vertex[4];
-	vertex[0].Position = DirectX::XMFLOAT3(pos.x - size.x * 0.5f, pos.y - size.y * 0.5f, 0.0f);
-	vertex[0].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[0].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[0].TexCoord = DirectX::XMFLOAT2(ul.x, ul.y);
+	vertex[0].Position = D3DXVECTOR3(pos.x - size.x * 0.5f, pos.y - size.y * 0.5f, 0.0f);
+	vertex[0].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[0].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[0].TexCoord = D3DXVECTOR2(ul.x, ul.y);
 
-	vertex[1].Position = DirectX::XMFLOAT3(pos.x + size.x * 0.5f, pos.y - size.y * 0.5f, 0.0f);
-	vertex[1].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[1].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[1].TexCoord = DirectX::XMFLOAT2(lr.x, ul.y);
+	vertex[1].Position = D3DXVECTOR3(pos.x + size.x * 0.5f, pos.y - size.y * 0.5f, 0.0f);
+	vertex[1].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[1].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[1].TexCoord = D3DXVECTOR2(lr.x, ul.y);
 
-	vertex[2].Position = DirectX::XMFLOAT3(pos.x - size.x * 0.5f, pos.y + size.y * 0.5f, 0.0f);
-	vertex[2].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[2].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[2].TexCoord = DirectX::XMFLOAT2(ul.x, lr.y);
+	vertex[2].Position = D3DXVECTOR3(pos.x - size.x * 0.5f, pos.y + size.y * 0.5f, 0.0f);
+	vertex[2].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[2].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[2].TexCoord = D3DXVECTOR2(ul.x, lr.y);
 
-	vertex[3].Position = DirectX::XMFLOAT3(pos.x + size.x * 0.5f, pos.y + size.y * 0.5f, 0.0f);
-	vertex[3].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[3].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[3].TexCoord = DirectX::XMFLOAT2(lr.x, lr.y);
+	vertex[3].Position = D3DXVECTOR3(pos.x + size.x * 0.5f, pos.y + size.y * 0.5f, 0.0f);
+	vertex[3].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[3].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[3].TexCoord = D3DXVECTOR2(lr.x, lr.y);
 
 	//頂点バッファ生成
 	D3D11_BUFFER_DESC bd;
@@ -149,7 +149,7 @@ void Renderer2D::Draw()
 
 	//マテリアル設定
 	Material material;
-	material.Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	material.Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
 	m_Graphics.SetMaterial(material);
 
 	//テクスチャ設定
@@ -165,9 +165,7 @@ void Renderer2D::Draw()
 void Renderer2D::Draw(float alpha)
 {
 	// シェーダーの設定
-	m_Resource.SetInputLayout("Default");
-	m_Resource.SetVertexShader("Default");
-	m_Resource.SetPixelShader("Default");
+	m_Resource.SetShader("Default");
 
 	//マトリクス設定
 	m_Graphics.SetWorldViewProjection2D();
@@ -179,7 +177,7 @@ void Renderer2D::Draw(float alpha)
 
 	//マテリアル設定
 	Material material;
-	material.Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, alpha);
+	material.Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, alpha);
 	m_Graphics.SetMaterial(material);
 
 	//テクスチャ設定
@@ -192,12 +190,10 @@ void Renderer2D::Draw(float alpha)
 	m_Graphics.GetDeviceContext()->Draw(4, 0);
 }
 
-void Renderer2D::Draw(const Math::Vector4& color)
+void Renderer2D::Draw(const D3DXVECTOR4& color)
 {
 	// シェーダーの設定
-	m_Resource.SetInputLayout("Default");
-	m_Resource.SetVertexShader("Default");
-	m_Resource.SetPixelShader("Default");
+	m_Resource.SetShader("Defualt");
 
 	//マトリクス設定
 	m_Graphics.SetWorldViewProjection2D();
@@ -209,7 +205,7 @@ void Renderer2D::Draw(const Math::Vector4& color)
 
 	//マテリアル設定
 	Material material;
-	material.Diffuse = Math::Vector4::CastXMFloat4(color);
+	material.Diffuse = D3DXVECTOR4(color);
 	m_Graphics.SetMaterial(material);
 
 	//テクスチャ設定
@@ -222,37 +218,35 @@ void Renderer2D::Draw(const Math::Vector4& color)
 	m_Graphics.GetDeviceContext()->Draw(4, 0);
 }
 
-void Renderer2D::Draw(const Math::Vector2& pos, const Math::Vector2& size, const Math::Vector2& ul, const Math::Vector2& lr, const Math::Vector4& color)
+void Renderer2D::Draw(const D3DXVECTOR2& pos, const D3DXVECTOR2& size, const D3DXVECTOR2& ul, const D3DXVECTOR2& lr, const D3DXVECTOR4& color)
 {
 	// シェーダーの設定
-	m_Resource.SetInputLayout("Default");
-	m_Resource.SetVertexShader("Default");
-	m_Resource.SetPixelShader("Default");
+	m_Resource.SetShader("NoLighting");
 
 	D3D11_MAPPED_SUBRESOURCE msr;
 	m_Graphics.GetDeviceContext()->Map(m_VertexBuffer.Get(), 0, D3D11_MAP_WRITE_DISCARD, 0, &msr);
 
 	Vertex3D* vertex = (Vertex3D*)msr.pData;
 
-	vertex[0].Position = DirectX::XMFLOAT3(pos.x - size.x * 0.5f, pos.y - size.y * 0.5f, 0.0f);
-	vertex[0].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[0].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[0].TexCoord = DirectX::XMFLOAT2(ul.x, ul.y);
+	vertex[0].Position = D3DXVECTOR3(pos.x - size.x * 0.5f, pos.y - size.y * 0.5f, 0.0f);
+	vertex[0].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[0].Diffuse = color;
+	vertex[0].TexCoord = D3DXVECTOR2(ul.x, ul.y);
 
-	vertex[1].Position = DirectX::XMFLOAT3(pos.x + size.x * 0.5f, pos.y - size.y * 0.5f, 0.0f);
-	vertex[1].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[1].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[1].TexCoord = DirectX::XMFLOAT2(lr.x, ul.y);
+	vertex[1].Position = D3DXVECTOR3(pos.x + size.x * 0.5f, pos.y - size.y * 0.5f, 0.0f);
+	vertex[1].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[1].Diffuse = color;
+	vertex[1].TexCoord = D3DXVECTOR2(lr.x, ul.y);
 
-	vertex[2].Position = DirectX::XMFLOAT3(pos.x - size.x * 0.5f, pos.y + size.y * 0.5f, 0.0f);
-	vertex[2].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[2].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[2].TexCoord = DirectX::XMFLOAT2(ul.x, lr.y);
+	vertex[2].Position = D3DXVECTOR3(pos.x - size.x * 0.5f, pos.y + size.y * 0.5f, 0.0f);
+	vertex[2].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[2].Diffuse = color;
+	vertex[2].TexCoord = D3DXVECTOR2(ul.x, lr.y);
 
-	vertex[3].Position = DirectX::XMFLOAT3(pos.x + size.x * 0.5f, pos.y + size.y * 0.5f, 0.0f);
-	vertex[3].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[3].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[3].TexCoord = DirectX::XMFLOAT2(lr.x, lr.y);
+	vertex[3].Position = D3DXVECTOR3(pos.x + size.x * 0.5f, pos.y + size.y * 0.5f, 0.0f);
+	vertex[3].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[3].Diffuse = color;
+	vertex[3].TexCoord = D3DXVECTOR2(lr.x, lr.y);
 
 	m_Graphics.GetDeviceContext()->Unmap(m_VertexBuffer.Get(), 0);
 
@@ -264,10 +258,6 @@ void Renderer2D::Draw(const Math::Vector2& pos, const Math::Vector2& size, const
 	UINT offset = 0;
 	m_Graphics.GetDeviceContext()->IASetVertexBuffers(0, 1, m_VertexBuffer.GetAddressOf(), &stride, &offset);
 
-	//マテリアル設定
-	Material material;
-	material.Diffuse = Math::Vector4::CastXMFloat4(color);
-	m_Graphics.SetMaterial(material);
 
 	//テクスチャ設定
 	m_Resource.SetTexture(0, m_Texture);
@@ -282,25 +272,25 @@ void Renderer2D::Draw(const Math::Vector2& pos, const Math::Vector2& size, const
 Render::Render(Graphics & graphics, Resource & resource) : m_Graphics(graphics), m_Resource(resource)
 {
 	Vertex3D vertex[4];
-	vertex[0].Position = DirectX::XMFLOAT3(-1.0f, 1.0f, 0.0f);
-	vertex[0].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[0].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[0].TexCoord = DirectX::XMFLOAT2(0.0f, 0.0f);
+	vertex[0].Position = D3DXVECTOR3(-1.0f, 1.0f, 0.0f);
+	vertex[0].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[0].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[0].TexCoord = D3DXVECTOR2(0.0f, 0.0f);
 
-	vertex[1].Position = DirectX::XMFLOAT3(1.0f, 1.0f, 0.0f);
-	vertex[1].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[1].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[1].TexCoord = DirectX::XMFLOAT2(1.0f, 0.0f);
+	vertex[1].Position = D3DXVECTOR3(1.0f, 1.0f, 0.0f);
+	vertex[1].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[1].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[1].TexCoord = D3DXVECTOR2(1.0f, 0.0f);
 
-	vertex[2].Position = DirectX::XMFLOAT3(-1.0f, -1.0f, 0.0f);
-	vertex[2].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[2].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[2].TexCoord = DirectX::XMFLOAT2(0.0f, 1.0f);
+	vertex[2].Position = D3DXVECTOR3(-1.0f, -1.0f, 0.0f);
+	vertex[2].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[2].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[2].TexCoord = D3DXVECTOR2(0.0f, 1.0f);
 
-	vertex[3].Position = DirectX::XMFLOAT3(1.0f, -1.0f, 0.0f);
-	vertex[3].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[3].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[3].TexCoord = DirectX::XMFLOAT2(1.0f, 1.0f);
+	vertex[3].Position = D3DXVECTOR3(1.0f, -1.0f, 0.0f);
+	vertex[3].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[3].Diffuse = D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f);
+	vertex[3].TexCoord = D3DXVECTOR2(1.0f, 1.0f);
 
 	//頂点バッファ生成
 	D3D11_BUFFER_DESC bd;
@@ -320,39 +310,37 @@ Render::~Render()
 {
 }
 
-void Render::Draw(float Param, const Math::Vector2 & pos, const Math::Vector4 & color)
+void Render::Draw(float Param, const D3DXVECTOR2 & pos, const D3DXVECTOR4 & color)
 {
 	D3D11_MAPPED_SUBRESOURCE msr;
 	m_Graphics.GetDeviceContext()->Map(m_VertexBuffer.Get(), 0, D3D11_MAP_WRITE_DISCARD, 0, &msr);
 
 	Vertex3D* vertex = (Vertex3D*)msr.pData;
 
-	vertex[0].Position = DirectX::XMFLOAT3(pos.x, pos.y - 10.0f, 0.0f);
-	vertex[0].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[0].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[0].TexCoord = DirectX::XMFLOAT2(0.0f, 0.0f);
+	vertex[0].Position = D3DXVECTOR3(pos.x, pos.y - 10.0f, 0.0f);
+	vertex[0].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[0].Diffuse = color;
+	vertex[0].TexCoord = D3DXVECTOR2(0.0f, 0.0f);
 
-	vertex[1].Position = DirectX::XMFLOAT3(pos.x + Param, pos.y - 10.0f, 0.0f);
-	vertex[1].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[1].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[1].TexCoord = DirectX::XMFLOAT2(1.0f, 0.0f);
+	vertex[1].Position = D3DXVECTOR3(pos.x + Param, pos.y - 10.0f, 0.0f);
+	vertex[1].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[1].Diffuse = color;
+	vertex[1].TexCoord = D3DXVECTOR2(1.0f, 0.0f);
 
-	vertex[2].Position = DirectX::XMFLOAT3(pos.x, pos.y + 10.0f, 0.0f);
-	vertex[2].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[2].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[2].TexCoord = DirectX::XMFLOAT2(0.0f, 1.0f);
+	vertex[2].Position = D3DXVECTOR3(pos.x, pos.y + 10.0f, 0.0f);
+	vertex[2].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[2].Diffuse = color;
+	vertex[2].TexCoord = D3DXVECTOR2(0.0f, 1.0f);
 
-	vertex[3].Position = DirectX::XMFLOAT3(pos.x + Param, pos.y + 10.0f, 0.0f);
-	vertex[3].Normal = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
-	vertex[3].Diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-	vertex[3].TexCoord = DirectX::XMFLOAT2(1.0f, 1.0f);
+	vertex[3].Position = D3DXVECTOR3(pos.x + Param, pos.y + 10.0f, 0.0f);
+	vertex[3].Normal = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+	vertex[3].Diffuse = color;
+	vertex[3].TexCoord = D3DXVECTOR2(1.0f, 1.0f);
 
 	m_Graphics.GetDeviceContext()->Unmap(m_VertexBuffer.Get(), 0);
 
 	// シェーダーの設定
-	m_Resource.SetInputLayout("Default");
-	m_Resource.SetVertexShader("Default");
-	m_Resource.SetPixelShader("Default");
+	m_Resource.SetShader("NoLighting");
 
 	//マトリクス設定
 	m_Graphics.SetWorldViewProjection2D();
@@ -361,11 +349,6 @@ void Render::Draw(float Param, const Math::Vector2 & pos, const Math::Vector4 & 
 	UINT stride = sizeof(Vertex3D);
 	UINT offset = 0;
 	m_Graphics.GetDeviceContext()->IASetVertexBuffers(0, 1, m_VertexBuffer.GetAddressOf(), &stride, &offset);
-
-	//マテリアル設定
-	Material material;
-	material.Diffuse = Math::Vector4::CastXMFloat4(color);
-	m_Graphics.SetMaterial(material);
 
 	//テクスチャ設定
 	m_Resource.SetTexture(0, "Gage");
